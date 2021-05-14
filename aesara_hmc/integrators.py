007998d2b9aad56365cfefb031ab0dc474644857
Rémi Louf <remilouf@gmail.com>
2021-05-14 16:46:22 +0200

    finish writing velocity_verlet tests

--- aesara_hmc/integrators.py
+++ aesara_hmc/integrators.py
@@ -15,7 +15,20 @@ def velocity_verlet(
     [TensorVariable, TensorVariable, TensorVariable, TensorVariable, TensorVariable],
     IntegratorStateType,
 ]:
+    """The velocity Verlet (or Verlet-Störmer) integrator.
 
+    The velocity Verlet is a two-stage palindromic integrator [1]_ of the form
+    (a1, b1, a2, b1, a1) with a1 = 0. It is numerically stable for values of
+    the step size that range between 0 and 2 (when the mass matrix is the
+    identity).
+
+    References
+    ----------
+    .. [1]: Bou-Rabee, Nawaf, and Jesús Marıa Sanz-Serna. "Geometric
+            integrators and the Hamiltonian Monte Carlo method." Acta Numerica 27
+            (2018): 113-206.
+
+    """
     a1 = 0
     b1 = 0.5
     a2 = 1 - 2 * a1
@@ -27,7 +40,6 @@ def velocity_verlet(
         potential_energy_grad: TensorVariable,
         step_size: TensorVariable,
     ) -> IntegratorStateType:
-        # position, momentum, _, potential_energy_grad = state
 
         new_momentum = momentum - b1 * step_size * potential_energy_grad
 
--- tests/test_integrators.py
+++ tests/test_integrators.py
@@ -7,7 +7,7 @@ from aesara.tensor.var import TensorVariable
 from aesara_hmc.integrators import velocity_verlet
 
 
-def HarmonicOscillator(inverse_mass_matrix, k=5, m=1.0):
+def HarmonicOscillator(inverse_mass_matrix, k=1.0, m=1.0):
     """Potential and Kinetic energy of an harmonic oscillator."""
 
     def potential_energy(x: TensorVariable) -> TensorVariable:
@@ -20,11 +20,11 @@ def HarmonicOscillator(inverse_mass_matrix, k=5, m=1.0):
     return potential_energy, kinetic_energy
 
 
-def FreeFall(inverse_mass_matrix, g=9.81, m=1.0):
+def FreeFall(inverse_mass_matrix, g=1.0):
     """Potential and kinetic energy of a free-falling object."""
 
     def potential_energy(h: TensorVariable) -> TensorVariable:
-        return aet.sum(m * g * h)
+        return aet.sum(g * h)
 
     def kinetic_energy(p: TensorVariable) -> TensorVariable:
         v = inverse_mass_matrix * p
@@ -33,22 +33,46 @@ def FreeFall(inverse_mass_matrix, g=9.81, m=1.0):
     return potential_energy, kinetic_energy
 
 
+def CircularMotion(inverse_mass_matrix):
+    def potential_energy(q: TensorVariable) -> TensorVariable:
+        return -1.0 / aet.power(aet.square(q[0]) + aet.square(q[1]), 0.5)
+
+    def kinetic_energy(p: TensorVariable) -> TensorVariable:
+        return 0.5 * aet.dot(inverse_mass_matrix, aet.square(p))
+
+    return potential_energy, kinetic_energy
+
+
 integration_examples = [
-    {
-        "model": HarmonicOscillator,
-        "n_steps": 100,
-        "step_size": 0.01,
-        "q_init": np.array([0.0]),
-        "p_init": np.array([1.0]),
-        "inverse_mass_matrix": np.array([1.0]),
-    },
     {
         "model": FreeFall,
         "n_steps": 100,
         "step_size": 0.01,
         "q_init": np.array([0.0]),
         "p_init": np.array([1.0]),
+        "q_final": np.array([0.5]),
+        "p_final": np.array([0.0]),
         "inverse_mass_matrix": np.array([1.0]),
+    },
+    {
+        "model": HarmonicOscillator,
+        "n_steps": 100,
+        "step_size": 0.01,
+        "q_init": np.array([0.0]),
+        "p_init": np.array([1.0]),
+        "q_final": np.array([np.sin(1.0)]),
+        "p_final": np.array([np.cos(1.0)]),
+        "inverse_mass_matrix": np.array([1.0]),
+    },
+    {
+        "model": CircularMotion,
+        "n_steps": 628,
+        "step_size": 0.01,
+        "q_init": np.array([1.0, 0.0]),
+        "p_init": np.array([0.0, 1.0]),
+        "q_final": np.array([1.0, 0.0]),
+        "p_final": np.array([0.0, 1.0]),
+        "inverse_mass_matrix": np.array([1.0, 1.0]),
     },
 ]
 
@@ -76,8 +100,6 @@ def create_integrate_fn(potential, step_fn, n_steps):
 
 @pytest.mark.parametrize("example", integration_examples)
 def test_velocity_verlet(example):
-    """The import characteristic of symplectic integrators is the conservation of energy."""
-
     model = example["model"]
     inverse_mass_matrix = example["inverse_mass_matrix"]
     step_size = example["step_size"]
@@ -89,16 +111,18 @@ def test_velocity_verlet(example):
 
     q = aet.vector("q")
     p = aet.vector("p")
-    # XXX: Should this be a matrix/column vector?
     p_final = aet.vector("p_final")
     energy_at = potential(q) + kinetic_energy(p)
-    energy_new_energy_fn = aesara.function(
-        (q, p, p_final), (energy_at, energy_at + kinetic_energy(p_final))
-    )
+    energy_fn = aesara.function((q, p), energy_at)
 
     integrate_fn = create_integrate_fn(potential, step, example["n_steps"])
     q_final, p_final, energy_final, _ = integrate_fn(q_init, p_init, step_size)
 
+    # Check that the trajectory was correctly integrated
+    np.testing.assert_allclose(example["q_final"], q_final[-1], atol=1e-2)
+    np.testing.assert_allclose(example["p_final"], p_final[-1], atol=1e-2)
+
     # Symplectic integrators conserve energy
-    energy, new_energy = energy_new_energy_fn(q_init, p_init, p_final.squeeze())
-    assert energy == pytest.approx(new_energy, 1e-3)
+    energy = energy_fn(q_init, p_init)
+    new_energy = energy_fn(q_final[-1], p_final[-1])
+    assert energy == pytest.approx(new_energy, 1e-4)
