0cc92f18f6614eee23f0e917fa8c25a2a5fe5cd5
Rémi Louf <remilouf@gmail.com>
2021-05-27 15:52:59 +0200

    add the HMC kernel

---
+++ aehmc/hmc.py
@@ -0,0 +1,102 @@
+from typing import Callable
+
+import aesara.tensor as aet
+import aesara_hmc.integrators as integrators
+import aesara_hmc.metrics as metrics
+import aesara_hmc.trajectory as trajectory
+import numpy as np
+from aesara.ifelse import ifelse
+from aesara.tensor.random.utils import RandomStream
+from aesara.tensor.var import TensorVariable
+
+
+def kernel(
+    srng: RandomStream,
+    potential_fn: Callable[[TensorVariable], TensorVariable],
+    step_size: TensorVariable,
+    inverse_mass_matrix: TensorVariable,
+    num_integration_steps: TensorVariable,
+    divergence_threshold: int = 1000,
+):
+
+    momentum_generator, kinetic_ernergy_fn, _ = metrics.gaussian_metric(
+        inverse_mass_matrix
+    )
+    symplectic_integrator = integrators.velocity_verlet(
+        potential_fn, kinetic_ernergy_fn
+    )
+    proposal_generator = hmc_proposal(
+        symplectic_integrator,
+        kinetic_ernergy_fn,
+        step_size,
+        num_integration_steps,
+        divergence_threshold,
+    )
+
+    def step(
+        q: TensorVariable,
+        potential_energy: TensorVariable,
+        potential_energy_grad: TensorVariable,
+    ):
+        p = momentum_generator(srng)
+        (
+            q_new,
+            p_new,
+            potential_energy_new,
+            potential_energy_grad_new,
+        ) = proposal_generator(srng, q, p, potential_energy, potential_energy_grad)
+        return q_new, potential_energy_new, potential_energy_grad_new
+
+    return step
+
+
+def hmc_proposal(
+    integrator: Callable,
+    kinetic_energy: Callable[[TensorVariable], TensorVariable],
+    step_size: TensorVariable,
+    num_integration_steps: TensorVariable,
+    divergence_threshold: int,
+):
+
+    integrate = trajectory.static_integration(
+        integrator, step_size, num_integration_steps
+    )
+
+    def propose(
+        srng: RandomStream,
+        q: TensorVariable,
+        p: TensorVariable,
+        potential_energy: TensorVariable,
+        potential_energy_grad: TensorVariable,
+    ):
+
+        new_q, new_p, new_potential_energy, new_potential_energy_grad = integrate(
+            q, p, potential_energy, potential_energy_grad
+        )
+
+        # flip the momentum to keep detailed balance
+        new_p = -1.0 * new_p
+
+        # compute transition-related quantities
+        energy = potential_energy + kinetic_energy(p)
+        new_energy = new_potential_energy + kinetic_energy(new_p)
+        delta_energy = energy - new_energy
+        delta_energy = aet.where(aet.isnan(delta_energy), -np.inf, delta_energy)
+        # is_transition_divergence = aet.abs(delta_energy) > divergence_threshold
+
+        p_accept = aet.clip(aet.exp(delta_energy), 0, 1.0)
+        do_accept = srng.bernoulli(p_accept)
+        (
+            final_q,
+            final_p,
+            final_potential_energy,
+            final_potential_energy_grad,
+        ) = ifelse(
+            do_accept,
+            (new_q, new_p, new_potential_energy, new_potential_energy_grad),
+            (q, p, potential_energy, potential_energy_grad),
+        )
+
+        return final_q, final_p, final_potential_energy, final_potential_energy_grad
+
+    return propose
---
+++ tests/test_hmc.py
@@ -0,0 +1,61 @@
+import aesara
+import aesara.tensor as aet
+import aesara_hmc.hmc as hmc
+import numpy as np
+from aesara.tensor.random.utils import RandomStream
+from aesara.tensor.var import TensorVariable
+
+
+def test_hmc():
+    def potential_fn(q: TensorVariable) -> TensorVariable:
+        return -1.0 / aet.power(aet.square(q[0]) + aet.square(q[1]), 0.5)
+
+    srng = RandomStream(seed=59)
+
+    step_size = aet.scalar("step_size")
+    inverse_mass_matrix = aet.vector("inverse_mass_matrix")
+    num_integration_steps = aet.scalar("num_integration_steps", dtype="int32")
+
+    q = aet.vector("q")
+    potential_energy = potential_fn(q)
+    potential_energy_grad = aesara.grad(potential_energy, wrt=q)
+
+    kernel = hmc.kernel(
+        srng, potential_fn, step_size, inverse_mass_matrix, num_integration_steps
+    )
+    next_step = kernel(q, potential_energy, potential_energy_grad)
+
+    # Compile a function that returns the next state
+    step_fn = aesara.function(
+        (q, step_size, inverse_mass_matrix, num_integration_steps), next_step
+    )
+
+    # Compile a function that integrates the trajectory integrating several times
+    trajectory, _ = aesara.scan(
+        fn=kernel,
+        outputs_info=[
+            {"initial": q},
+            {"initial": potential_energy},
+            {"initial": potential_energy_grad},
+        ],
+        n_steps=1000,
+    )
+    traj = aesara.function(
+        (q, step_size, inverse_mass_matrix, num_integration_steps), trajectory
+    )
+
+    # Run
+    step_size = 0.01
+    num_integration_steps = 3
+    q = np.array([1.0, 1.0])
+    inverse_mass_matrix = np.array([1.0, 1.0])
+
+    # This works
+    samples = []
+    for _ in range(1_000):
+        q, *_ = step_fn(q, step_size, inverse_mass_matrix, num_integration_steps)
+        samples.append(q)
+    print(np.mean(np.array(samples)))
+
+    # This doesn't
+    print(traj(q, step_size, inverse_mass_matrix, num_integration_steps))
