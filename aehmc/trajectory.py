48c3b985f9d66d53076cf6ce87767f1d58ee88ce
Rémi Louf <remilouf@gmail.com>
2021-08-23 22:50:08 +0200

    Use aeppl to build the logprob

--- aehmc/trajectory.py
+++ aehmc/trajectory.py
@@ -35,15 +35,14 @@ def static_integration(
     integrator: Callable,
     step_size: float,
     num_integration_steps: int,
-    direction: int = 1,
 ) -> Callable:
     """Generate a trajectory by integrating several times in one direction."""
 
-    directed_step_size = direction * step_size
-
     def integrate(q_init, p_init, energy_init, energy_grad_init) -> IntegratorStateType:
-        def one_step(q, p, energy, energy_grad):
-            new_state = integrator(q, p, energy, energy_grad, directed_step_size)
+        def one_step(q, p, potential_energy, potential_energy_grad):
+            new_state = integrator(
+                q, p, potential_energy, potential_energy_grad, step_size
+            )
             return new_state
 
         [q, p, energy, energy_grad], _ = aesara.scan(
