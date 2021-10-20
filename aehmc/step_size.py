29f8789aebef4a3e87e8c9669297c57cde871589
Rémi Louf <remilouf@gmail.com>
2021-10-20 18:31:06 +0200

    Refactor test for step size adaptation

--- aehmc/step_size.py
+++ aehmc/step_size.py
@@ -9,14 +9,72 @@ from aehmc import algorithms
 
 
 def dual_averaging_adaptation(
-    initial_step_size: TensorVariable,
+    initial_log_step_size: TensorVariable,
     target_acceptance_rate: TensorVariable = at.as_tensor(0.65),
     gamma: float = 0.05,
     t0: int = 10,
     kappa: float = 0.75,
 ) -> Tuple[Callable, Callable]:
+    """Tune the step size to achieve a desired target acceptance rate.
 
-    mu = at.log(10) + at.log(initial_step_size)
+    Let us note :math:`\\epsilon` the current step size, :math:`\\alpha_t` the
+    metropolis acceptance rate at time :math:`t` and :math:`\\delta` the desired
+    aceptance rate. We define:
+
+    .. math:
+        H_t = \\delta - \\alpha_t
+
+    the error at time t. We would like to find a procedure that adapts the
+    value of :math:`\\epsilon` such that :math:`h(x) =\\mathbb{E}\\left[H_t|\\epsilon\\right] = 0`
+    Following [1]_, the authors of [2]_ proposed the following update scheme. If
+    we note :math:``x = \\log \\epsilon` we follow:
+
+    .. math:
+        x_{t+1} \\LongLeftArrow \\mu - \\frac{\\sqrt{t}}{\\gamma} \\frac{1}{t+t_0} \\sum_{i=1}^t H_i
+        \\overline{x}_{t+1} \\LongLeftArrow x_{t+1}\\, t^{-\\kappa}  + \\left(1-t^\\kappa\\right)\\overline{x}_t
+
+    :math:`\\overline{x}_{t}` is guaranteed to converge to a value such that
+    :math:`h(\\overline{x}_t)` converges to 0, i.e. the Metropolis acceptance
+    rate converges to the desired rate.
+
+    See reference [2]_ (section 3.2.1) for a detailed discussion.
+
+    Parameters
+    ----------
+    initial_log_step_size:
+        Initial value of the logarithm of the step size, used as an iterate in
+        the dual averaging algorithm.
+    target_acceptance_rate:
+        Target acceptance rate.
+    gamma
+        Controls the speed of convergence of the scheme. The authors of [2]_ recommend
+        a value of 0.05.
+    t0: float >= 0
+        Free parameter that stabilizes the initial iterations of the algorithm.
+        Large values may slow down convergence. Introduced in [2]_ with a default
+        value of 10.
+    kappa: float in ]0.5, 1]
+        Controls the weights of past steps in the current update. The scheme will
+        quickly forget earlier step for a small value of `kappa`. Introduced
+        in [2]_, with a recommended value of .75
+
+    Returns
+    -------
+    init
+        A function that initializes the state of the dual averaging scheme.
+    update
+        A function that updates the state of the dual averaging scheme.
+
+    References
+    ----------
+    .. [1]: Nesterov, Yurii. "Primal-dual subgradient methods for convex
+            problems." Mathematical programming 120.1 (2009): 221-259.
+    .. [2]: Hoffman, Matthew D., and Andrew Gelman. "The No-U-Turn sampler:
+            adaptively setting path lengths in Hamiltonian Monte Carlo." Journal
+            of Machine Learning Research 15.1 (2014): 1593-1623.
+    """
+
+    mu = at.log(10) + initial_log_step_size
     da_init, da_update = algorithms.dual_averaging(mu, gamma, t0, kappa)
 
     def update(
@@ -39,6 +97,43 @@ def heuristic_adaptation(
     target_acceptance_rate=0.65,
     max_num_iterations=100,
 ):
+    """Find a reasonable initial step size during warmup.
+
+    While the dual averaging scheme is guaranteed to converge to a reasonable
+    value for the step size starting from any value, choosing a good first
+    value can speed up the convergence. This heuristics doubles and halves the
+    step size until the acceptance probability of the HMC proposal crosses the
+    target value.
+
+    Parameters
+    ----------
+    kernel
+        A function that takes a state, a step size and returns a new state.
+    reference_hmc_state
+        The location (HMC state) where this first step size must be found. This function
+        never advances the chain.
+    inverse_mass_matrix
+        The inverse mass matrix relative to which the step size must be found.
+    initial_step_size
+        The first step size used to start the search.
+    target_acceptance_rate
+        Once that value of the metropolis acceptance probability is reached we
+        estimate that we have found a "reasonable" first step size.
+    max_num_iterations
+        The maximum number of times we iterate on the algorithm.
+
+    Returns
+    -------
+    float
+        A reasonable first value for the step size.
+
+    Reference
+    ---------
+    .. [1]: Hoffman, Matthew D., and Andrew Gelman. "The No-U-Turn sampler:
+            adaptively setting path lengths in Hamiltonian Monte Carlo." Journal
+            of Machine Learning Research 15.1 (2014): 1593-1623.
+    """
+
     def update(step_size, direction, previous_direction):
         step_size = (2.0 ** direction) * step_size
         *_, p_accept = kernel(*reference_state, step_size)
--- tests/test_step_size.py
+++ tests/test_step_size.py
@@ -8,50 +8,43 @@ from aehmc import hmc
 from aehmc.step_size import dual_averaging_adaptation, heuristic_adaptation
 
 
-def test_heuristic_adaptation():
+@pytest.fixture()
+def init():
     def logprob_fn(x):
-        return -at.sum(0.5 * x)
+        return -2 * (x - 1.0) ** 2
 
     srng = RandomStream(seed=0)
-
-    initial_position = at.as_tensor(1.0, dtype="floatX")
-    logprob = logprob_fn(initial_position)
-    logprob_grad = aesara.grad(logprob, wrt=initial_position)
-    reference_state = (initial_position, logprob, logprob_grad)
-
     inverse_mass_matrix = at.as_tensor(1.0)
-
     kernel = hmc.kernel(srng, logprob_fn, inverse_mass_matrix, 10)
 
+    initial_position = at.as_tensor(1.0, dtype="floatX")
+    initial_state = hmc.new_state(initial_position, logprob_fn)
+
+    return initial_state, kernel
+
+
+def test_heuristic_adaptation(init):
+    reference_state, kernel = init
+
     epsilon_1 = heuristic_adaptation(
-        kernel, reference_state, at.as_tensor(1, dtype="floatX"), 0.95
+        kernel, reference_state, at.as_tensor(0.5, dtype="floatX"), 0.95
     )
     epsilon_1_val = epsilon_1.eval()
     assert epsilon_1_val != np.inf
 
     epsilon_2 = heuristic_adaptation(
-        kernel, reference_state, at.as_tensor(1, dtype="floatX"), 0.05
+        kernel, reference_state, at.as_tensor(0.5, dtype="floatX"), 0.05
     )
     epsilon_2_val = epsilon_2.eval()
     assert epsilon_2_val > epsilon_1_val
     assert epsilon_2_val != np.inf
 
 
-def test_dual_averaging_adaptation():
-    def logprob_fn(x):
-        return -at.sum(0.5 * x ** 2)
+def test_dual_averaging_adaptation(init):
+    initial_state, kernel = init
 
-    srng = RandomStream(seed=0)
-    inverse_mass_matrix = at.as_tensor(1.0)
-    kernel = hmc.kernel(srng, logprob_fn, inverse_mass_matrix, 10)
-
-    initial_position = at.as_tensor(1.0, dtype="floatX")
-    logprob = logprob_fn(initial_position)
-    logprob_grad = aesara.grad(logprob, wrt=initial_position)
-
-    step_size = at.as_tensor(1.0, dtype="floatX")
-    logpstepsize = at.log(step_size)
-    init, update = dual_averaging_adaptation(step_size)
+    logpstepsize = at.log(at.as_tensor(1.0, dtype="floatX"))
+    init, update = dual_averaging_adaptation(logpstepsize)
     step, logstepsize_avg, gradient_avg = init(at.as_tensor(0.0, dtype="floatX"))
 
     def one_step(q, logprob, logprob_grad, step, x_t, x_avg, gradient_avg):
@@ -62,9 +55,9 @@ def test_dual_averaging_adaptation():
     states, updates = aesara.scan(
         fn=one_step,
         outputs_info=[
-            {"initial": initial_position},
-            {"initial": logprob},
-            {"initial": logprob_grad},
+            {"initial": initial_state[0]},
+            {"initial": initial_state[1]},
+            {"initial": initial_state[2]},
             {"initial": step},
             {"initial": logpstepsize},
             {"initial": logstepsize_avg},
@@ -78,4 +71,4 @@ def test_dual_averaging_adaptation():
     step_size = aesara.function((), at.exp(states[-3][-1]), updates=updates)
     assert np.mean(p_accept()) == pytest.approx(0.65, rel=10e-3)
     assert step_size() < 10
-    assert step_size() > 10e-1
+    assert step_size() > 1e-1
