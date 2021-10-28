8408a16968efba9ab1352833c18591ef5136c1fa
Rémi Louf <remilouf@gmail.com>
2021-10-28 17:27:45 +0200

    Simplify the HMC sampling test

--- tests/test_hmc.py
+++ tests/test_hmc.py
@@ -1,9 +1,8 @@
-from typing import Callable
-
 import aesara
-import aesara.tensor as aet
+import aesara.tensor as at
 import numpy as np
 import pytest
+from aeppl import joint_logprob
 from aesara.tensor.random.utils import RandomStream
 from aesara.tensor.var import TensorVariable
 
@@ -11,30 +10,66 @@ from aehmc import hmc, nuts
 
 
 def normal_logprob(q: TensorVariable):
-    return -aet.sum(aet.square(q - 3.0))
+    y = (q - 3.0) / 5.0
+    return -at.sum(at.square(y))
 
 
-def normal_potential(q: TensorVariable):
-    return -normal_logprob(q)
+def test_hmc():
+    """Test the HMC kernel on a gaussian target."""
+    step_size = 0.04
+    inverse_mass_matrix = at.as_tensor(1.0)
+    num_integration_steps = 20
 
+    Y_rv = at.random.normal(1, 2)
 
-def build_hmc_trajectory_generator(
-    srng: RandomStream,
-    kernel_generator: Callable,
-    state_generator: Callable,
-    logprob_fn: Callable,
-    num_states: int,
-) -> Callable:
-    q = aet.vector("q")
-    initial_state = state_generator(q, logprob_fn)
+    def logprob_fn(y):
+        logprob = joint_logprob({Y_rv: y})
+        return logprob
 
-    step_size = aet.scalar("step_size")
-    inverse_mass_matrix = aet.vector("inverse_mass_matrix")
-    num_integration_steps = aet.scalar("num_integration_steps", dtype="int32")
-
-    kernel = kernel_generator(
-        srng, logprob_fn, inverse_mass_matrix, num_integration_steps
+    srng = RandomStream(seed=0)
+    kernel = hmc.kernel(
+        srng,
+        logprob_fn,
+        inverse_mass_matrix,
+        num_integration_steps,
     )
+
+    y_vv = Y_rv.clone()
+    initial_state = hmc.new_state(y_vv, logprob_fn)
+
+    trajectory, updates = aesara.scan(
+        kernel,
+        outputs_info=[
+            {"initial": initial_state[0]},
+            {"initial": initial_state[1]},
+            {"initial": initial_state[2]},
+            None,
+        ],
+        non_sequences=step_size,
+        n_steps=20_000,
+    )
+
+    trajectory_generator = aesara.function((y_vv,), trajectory[0], updates=updates)
+
+    samples = trajectory_generator(3.0)
+    assert np.mean(samples[1000:]) == pytest.approx(1.0, rel=1e-1)
+    assert np.var(samples[1000:]) == pytest.approx(4.0, rel=1e-1)
+
+
+def logprob(q):
+    return -at.sum(0.5 * at.square((q - 3) / 2))
+
+
+def test_nuts():
+    """Test the NUTS kernel on a gaussian target."""
+    srng = RandomStream(seed=59)
+
+    step_size = at.scalar("step_size", dtype="float64")
+    inverse_mass_matrix = at.vector("inverse_mass_matrix", dtype="float64")
+    kernel = nuts.kernel(srng, normal_logprob, inverse_mass_matrix)
+
+    q = at.vector("q")
+    initial_state = nuts.new_state(q, normal_logprob)
 
     trajectory, updates = aesara.scan(
         fn=kernel,
@@ -43,66 +78,23 @@ def build_hmc_trajectory_generator(
             {"initial": initial_state[1]},
             {"initial": initial_state[2]},
             None,
+            None,
         ],
         non_sequences=step_size,
-        n_steps=num_states,
+        n_steps=1000,
     )
+
     trajectory_generator = aesara.function(
-        (q, step_size, inverse_mass_matrix, num_integration_steps),
+        (q, step_size, inverse_mass_matrix),
         trajectory,
         updates=updates,
     )
 
-    return trajectory_generator
-
-
-def test_hmc():
-    """Test the HMC kernel on a simple potential."""
-    srng = RandomStream(seed=59)
-    step_size = 0.003
-    num_integration_steps = 10
-    initial_position = np.array([1.0])
-    inverse_mass_matrix = np.array([1.0])
-
-    trajectory_generator = build_hmc_trajectory_generator(
-        srng, hmc.kernel, hmc.new_state, normal_logprob, 50_000
-    )
-    positions, *_ = trajectory_generator(
-        initial_position, step_size, inverse_mass_matrix, num_integration_steps
-    )
-
-    assert np.mean(positions[10_000:], axis=0) == pytest.approx(3, 1e-1)
-
-
-def test_nuts():
-    """Test the NUTS kernel on a simple potential."""
-    srng = RandomStream(seed=59)
-
-    q = aet.vector("q")
-    initial_state = nuts.new_state(q, normal_logprob)
-
-    step_size = aet.scalar("step_size", dtype="float64")
-    inverse_mass_matrix = aet.vector("inverse_mass_matrix", dtype="float64")
-
-    kernel = nuts.kernel(srng, normal_logprob, inverse_mass_matrix)
-    result, updates = kernel(*initial_state, step_size)
-
-    trajectory_generator = aesara.function(
-        (q, step_size, inverse_mass_matrix),
-        result,
-        updates=updates,
-        allow_input_downcast=True,
-    )
-
     step_size = 0.01
-    initial_position = np.array([1.0])
     inverse_mass_matrix = np.array([1.0])
+    initial_position = np.array([1.0])
+    trajectory = trajectory_generator(initial_position, step_size, inverse_mass_matrix)
+    samples = trajectory[0][300:]
 
-    q = initial_position
-    positions = []
-    for _ in range(1000):
-        result = trajectory_generator(q, step_size, inverse_mass_matrix)
-        q = result[0]
-        positions.append(q)
-
-    assert np.mean(np.array(positions)[300:]) == pytest.approx(3, 1e-1)
+    assert np.mean(np.array(samples)) == pytest.approx(3, 1e-1)
+    print(np.var(samples))
